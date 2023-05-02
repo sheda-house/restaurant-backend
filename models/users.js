745401de47012fda4084ca29f0844296cbef8976
zionnaire <zionnaire2018@gmail.com>
2023-05-02 15:46:11 +0100

    roles added

---
+++ models/users.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const userSchema = new mongoose.Schema({
+    FirstName : String,
+    LastName : String,
+    Email: {
+        type: String,
+        unique : true
+    },
+    PhoneNumber: {
+        type: String,
+        unique: true
+    },
+    Password: String,
+    RoleId: {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Role"
+    }
+})
+
+module.exports = mongoose.model("User", userSchema)
